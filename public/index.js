42d83c73370de6d4e0dedf354f47bb92f990cb0c
Andrew Fawcett <141191160+Foocett@users.noreply.github.com>
2024-05-15 11:18:09 -0500

    Added system clock

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    const scrambleText = document.getElementById("scramble-text");
+    scrambleText.textContent = Scramble3();
 })
 
 function Scramble3() {
@@ -23,4 +24,21 @@ function Scramble3() {
     }
 
     return scramble.join(' ');
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let ampm = " " +  (h >= 12 ? 'PM' : 'AM');
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s + ampm;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    return i;
 }
